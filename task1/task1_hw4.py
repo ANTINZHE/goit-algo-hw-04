5f1e5b5dcdd627a4fe5282874e4aef8f44bca82f
ANTIN <antonzhelihovskiy@gmail.com>
2025-08-01 23:06:48 +0300

    make second task, and rebuild first task

--- task1/task1_hw4.py
+++ task1/task1_hw4.py
@@ -1,31 +1,20 @@
-from pathlib import Path
+def total_salary(path: str) -> tuple[int, int]:
+    total_sum = 0
+    number_of_workers = 0
 
-def total_salary(path: Path) -> tuple[int, int]:
-    if path.exists() and path.is_file():
-
-        total_sum = 0
-        number_of_workers = 0
-
+    try:
         with open(path, "r") as file:
             for line in file:
-                line = line.strip()
-                name, payment_str = line.split(",")
+                name, payment_str = line.strip().split(",")
                 payment = int(payment_str)
                 total_sum += payment
                 number_of_workers += 1
 
         middle_payment = total_sum / number_of_workers
         return total_sum, int(middle_payment)
-    else:
-        print("File not found.")
 
-# Створення файлу з оплатами
-list_of_payments = "Alex Korp,6000\nNikita Borisenko,2000\nSitarama Raju,1000"
-with open('list_of payments.txt', "w") as file:
-    file.write(list_of_payments)
+    except FileNotFoundError:
+        return "Файл не знайдено"
 
-path = Path("list_of payments.txt")
-
-# Виклик функції
-total, average = total_salary(path)
+total, average = total_salary("list_of payments.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
---
+++ task2/task2_hw4.py
@@ -0,0 +1,15 @@
+def get_cats_info(path):
+    list_of_cats = []
+
+    try:
+        with open(path, 'r', encoding="UTF-8") as file:
+            cats = file.readlines()
+            cats_info = [cat.strip().split(",") for cat in cats if cat.strip()]
+            for index in cats_info:
+                list_of_cats.append(dict(id=index[0], name=index[1], age=index[2]))
+            return list_of_cats
+
+    except FileNotFoundError:
+        return "Файл не знайдено"
+cats_info = get_cats_info("cats_info_file.txt")
+print(cats_info)
