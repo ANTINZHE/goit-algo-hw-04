76873659a79c3ce6eaae1388e11d7b6a61360cb3
ANTIN <antonzhelihovskiy@gmail.com>
2025-08-03 12:12:11 +0300

    int to float division by zero Error filenotfound

--- task1/task1_hw4.py
+++ task1/task1_hw4.py
@@ -1,4 +1,4 @@
-def total_salary(path: str) -> tuple[int, int]:
+def total_salary(path: str) -> tuple[float, float]:
     total_sum = 0
     number_of_workers = 0
 
@@ -6,15 +6,17 @@ def total_salary(path: str) -> tuple[int, int]:
         with open(path, "r", encoding="UTF-8") as file:
             for line in file:
                 name, payment_str = line.strip().split(",")
-                payment = int(payment_str)
+                payment = float(payment_str)
                 total_sum += payment
                 number_of_workers += 1
+            average = total_sum / number_of_workers if number_of_workers else 0
 
-        average = total_sum / number_of_workers
-        return total_sum, int(average)
+        return total_sum, average
 
     except FileNotFoundError:
-        return "Файл не знайдено"
+        print("Файл не знайдено")
+        return None, None
 
-total, average = total_salary("list_of payments.txt")
+
+total, average = total_salary("list_of paymintents.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
